39c7e4e7d1f4f1aaff8086d61c56d1dab3a147cf
dLogan807 <dylanlogan316@gmail.com>
2023-04-28 15:00:47 +1200

    Implemented login and registering functionality

--- Assets/Scripts/PlayFabManager.cs
+++ Assets/Scripts/PlayFabManager.cs
@@ -5,14 +5,26 @@ using PlayFab;
 using PlayFab.ClientModels;
 using UnityEditor.PackageManager;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayFabManager : MonoBehaviour
 {
-    public Text messageText;
-    public InputField emailInput, passwordInput;
+    public TMP_Text messageText;
+    public TMP_InputField emailInput, passwordInput;
+    private Color red = new Color32(231, 112, 112, 255);
+    private Color green = new Color32(112, 231, 115, 255);
 
-    public void SignUpButton()
+    //When the register button is clicked
+    public void RegisterButton()
     {
+        if (passwordInput.text.Length < 6)
+        {
+            messageText.color = red;
+            messageText.text = "Password must be at least 6 characters";
+            return;
+        }
+
+        //Form request
         var request = new RegisterPlayFabUserRequest
         {
             Email = emailInput.text,
@@ -20,32 +32,58 @@ public class PlayFabManager : MonoBehaviour
             RequireBothUsernameAndEmail = false
         };
 
+        //Send register request
         PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnError);
+    }
+
+    //When the login button is clicked
+    public void LoginButton()
+    {
+        //Form request
+        var request = new LoginWithEmailAddressRequest 
+        { 
+            Email = emailInput.text,
+            Password = passwordInput.text
+        };
+
+        //Send login request
+        PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnError);
+    }
+
+    //When the reset password button is clicked
+    public void ResetPasswordButton()
+    {
+        var request = new SendAccountRecoveryEmailRequest
+        {
+            Email = emailInput.text,
+            TitleId = "AB628"
+        };
+
+        PlayFabClientAPI.SendAccountRecoveryEmail(request, OnPasswordReset, OnError);
     }
 
     private void OnRegisterSuccess(RegisterPlayFabUserResult result)
     {
+        messageText.color = green;
         messageText.text = "Successfully registered and logged in!";
     }
 
-    /*public void Login()
+    private void OnLoginSuccess(LoginResult result)
     {
-        var request = new LoginWithCustomIDRequest
-        {
-            CustomId = SystemInfo.deviceUniqueIdentifier,
-            CreateAccount = true
-        };
-        PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
+        messageText.color = green;
+        messageText.text = "Successfully logged in!";
     }
 
-    private void OnSuccess(LoginResult result)
+    private void OnPasswordReset(SendAccountRecoveryEmailResult result)
     {
-        Debug.Log("Successfully logged in!");
-    }*/
+        messageText.color = green;
+        messageText.text = "Password reset email sent";
+    }
 
     private void OnError(PlayFabError error)
     {
-        Debug.Log("An error occured while logging in!");
+        messageText.color = red;
+        messageText.text = error.ErrorMessage;
         Debug.Log(error.GenerateErrorReport());
     }
 }
