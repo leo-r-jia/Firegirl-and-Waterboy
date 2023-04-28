f4ac7a8812df33db64183a1d5911ee7399b26b40
dLogan807 <dylanlogan316@gmail.com>
2023-04-28 17:52:40 +1200

    Redid authentication to use usernames
    Rather than emails. Benefits of using emails would be to easily reset an account's password

--- Assets/Scripts/PlayFabManager.cs
+++ Assets/Scripts/PlayFabManager.cs
@@ -11,14 +11,24 @@ using UnityEngine.Events;
 public class PlayFabManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text messageText;
-    [SerializeField] private TMP_InputField emailInput, passwordInput;
+    [SerializeField] private TMP_InputField usernameInput, passwordInput;
     private Color red = new Color32(231, 112, 112, 255);
     private Color green = new Color32(112, 231, 115, 255);
 
     public UnityEvent LoggedIn;
 
+    /*public void Start()
+    {
+        var request = new GetAccountInfoRequest
+        {
+
+        }
+
+        Debug.Log(PlayFabClientAPI.getAcc);
+    }*/
+
     //When the register button is clicked
-    public void RegisterButton()
+    public void CreateAccountButton()
     {
         if (passwordInput.text.Length < 6)
         {
@@ -27,10 +37,10 @@ public class PlayFabManager : MonoBehaviour
             return;
         }
 
-        //Form request
+        //Form request for registration with username + password
         var request = new RegisterPlayFabUserRequest
         {
-            Email = emailInput.text,
+            Username = usernameInput.text,
             Password = passwordInput.text,
             RequireBothUsernameAndEmail = false
         };
@@ -42,15 +52,15 @@ public class PlayFabManager : MonoBehaviour
     //When the login button is clicked
     public void LoginButton()
     {
-        //Form request
-        var request = new LoginWithEmailAddressRequest 
+        //Form request for login with username + password
+        var request = new LoginWithPlayFabRequest 
         { 
-            Email = emailInput.text,
+            Username = usernameInput.text,
             Password = passwordInput.text
         };
 
         //Send login request
-        PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnError);
+        PlayFabClientAPI.LoginWithPlayFab(request, OnLoginSuccess, OnError);
     }
 
     private void OnRegisterSuccess(RegisterPlayFabUserResult result)
