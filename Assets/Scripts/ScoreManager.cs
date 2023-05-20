6ad2f29061cfc9cac607a69ac134a4cae6346012
Leo Jia <leo.r.jia@outlook.com>
2023-05-20 21:25:18 +1200

    Level Complete panel
    Upgraded Level Complete panel to display score and coins collected. Move LevelComplete script out of Exit and instead to a new game object LevelCompleteListener. Cleaned up Timer script

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,11 +6,16 @@ using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
+    //Live coin count
     private int coins = 0;
     [SerializeField] public TextMeshProUGUI coinsCollected;
 
+    //Fields displayed upon level completion
+    [SerializeField] public TextMeshProUGUI coinsCollectedLevelComplete;
+    [SerializeField] public TextMeshProUGUI scoreLevelComplete;
+
     [SerializeField] public Timer timer;
-    public int score;
+    private int score = 0;
 
     //Update coins collected
     public void UpdateScore()
@@ -19,12 +24,20 @@ public class ScoreManager : MonoBehaviour
         coinsCollected.text = "Coins: " + coins;
     }
 
+    //Level complete
+    public void LevelComplete()
+    {
+        CalculateFinalScore();
+        coinsCollectedLevelComplete.text = coins + "/6";
+        scoreLevelComplete.text = score +"";
+    }
+
     //Calculate score on level complete
-    public void calculateFinalScore()
+    private void CalculateFinalScore()
     {
         score = 0;
         //Calculate final score based on coins collected and time taken
-        score += coins * 10000 + 5000 - (int)Math.Ceiling(timer.currentTime);
+        score = coins * 10000 + (int)((5000 - timer.GetTime()) * (1 + 0.01 * coins));
     }
 
     //Add the coins collected this level to the player
