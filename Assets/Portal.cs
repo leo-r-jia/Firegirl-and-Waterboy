615a9dd85b8515ba63e0b27532e3c8110a52e9e4
jWaitoa <jackwaitoa@gmail.com>
2023-05-21 16:06:56 +1200

    Created a portal script
    Currently has a few bugs such as getting stuck by repeatedly teleporting

---
+++ Assets/Portal.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    private Transform destination;
+
+    public bool isOrange;
+    public float distance = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(isOrange == false)
+        {
+            destination = GameObject.FindGameObjectWithTag("Orange Portal").GetComponent<Transform>();
+        }
+        else
+        {
+            destination = GameObject.FindGameObjectWithTag("Blue Portal").GetComponent<Transform>();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (Vector2.Distance(transform.position, other.transform.position) > distance)
+        {
+            StartCoroutine(Waiter(other));
+        }
+    }
+
+    private IEnumerator Waiter(Collider2D other)
+    {
+        yield return new WaitForSeconds(0.2f);
+        other.transform.position = new Vector2(destination.position.x+2, destination.position.y+2);
+        other.GetComponent<Rigidbody2D>().AddForce(new Vector2(200f, 2f));
+
+    }
+}
