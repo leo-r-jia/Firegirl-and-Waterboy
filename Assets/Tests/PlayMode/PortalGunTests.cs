40cec62334b43f9db34dd74c21e281811570f7c4
jWaitoa <jackwaitoa@gmail.com>
2023-06-06 16:59:26 +1200

    portal gun change

--- Assets/Tests/PlayMode/PortalGunTests.cs
+++ Assets/Tests/PlayMode/PortalGunTests.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class PortalGunTests : MonoBehaviour
 {
+    private Transform PortalGun;
 
-/*    [UnitySetUp]
-    public IEnumerator SetUp()
-    {
-        SceneManager.LoadScene("Scenes/Jack/Level One");
+    /*    [UnitySetUp]
+        public IEnumerator SetUp()
+        {
+            SceneManager.LoadScene("Scenes/Jack/Level One");
 
-        yield return null;
+            yield return null;
 
-        GameObject canvas = GameObject.Find("Canvas"); // Look for game object
-        Transform LevelMenu = canvas.transform.Find("LevelMenu");
-    }*/
+            GameObject canvas = GameObject.Find("Canvas"); // Look for game object
+            Transform LevelMenu = canvas.transform.Find("LevelMenu");
+        }*/
 
     // Player press shoot key, bullet should exist.
+
+    /*[Test]
+    public void CreatePortalBullet_WhenExecuted_CreatesPortalBullet()
+    {
+        int level = 1;
+
+        PortalGun.gameObject.GetComponent<LevelView>().SetPlayerDataCurrentLevel(level);
+
+        int actual = PlayerData.Instance.CurrentLevel + 1;
+
+        Assert.AreEqual(level, actual);
+    }*/
 }
